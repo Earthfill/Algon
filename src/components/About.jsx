96aedaf97c29c426c0adcfc2ddd28e0009bb578c
Kingsley Omoaka-Iyorah <kingsley.omiy@gmail.com>
2023-11-21 17:54:55 +0100

    desktop view

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -19,15 +19,15 @@ const About = () => {
       </div>
       <div className="px-12 text-sm text-slate-600">
         <div className="text-center">Check the registration status of your large vehicle. Simply input your vehicle registration number in the form above or scan the QR code on the vehicle.</div>
-        <div className="flex gap-4 text-slate-600 pt-7">
+        <div className="flex xl:justify-center items-start gap-4 text-slate-600 pt-7">
           <span className="text-red text-4xl"><FaSearchengin /></span>
           <span className="justify-start">Searches the vehicle registration number in the database</span>
         </div>
-        <div className="flex gap-4 text-slate-600 pt-7">
+        <div className="flex xl:justify-center items-start gap-4 text-slate-600 pt-7">
           <span className="text-red text-4xl"><FaCheck /></span>
           <span className="justify-start">Shows the vehicles registration status</span>
         </div>
-        <div className="flex gap-4 text-slate-600 pt-7">
+        <div className="flex xl:justify-center items-start  gap-4 text-slate-600 pt-7">
           <span className="text-red text-4xl"><GiSandsOfTime /></span>
           <span className="justify-start">Works 24 hours a day, 7 days a week. Even on holidays!</span>
         </div>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className="py-3 mt-20 bg-royal-red text-white text-center">
+    <div className="py-3 xl:mt-12 fixed bottom-0 w-full mt-20 bg-royal-red text-white text-center">
       Registration status checker
     </div>
   )
