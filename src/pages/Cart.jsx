f8ff2a4c709f5ca49c347ec7c068d9552b11b659
Kingsley Omoaka-Iyorah <kingsley.omiy@gmail.com>
2023-12-22 17:21:22 +0100

    certificate card

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,9 +1,10 @@
 import { ImArrowLeft2 } from "react-icons/im"
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import CartItems from "../components/CartItems";
 import { FaLongArrowAltRight } from "react-icons/fa";
 
 const Cart = () => {
+  const navigate = useNavigate();
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
   const cartFromQuery = JSON.parse(queryParams.get("cart")) || [];
@@ -15,12 +16,14 @@ const Cart = () => {
   return (
     <div className="p-6">
       <div className="flex gap-3 items-center pb-5">
-        <div className="border rounded-full p-2"><ImArrowLeft2 /></div>
+        <button onClick={() => navigate(-1)} className="border rounded-full p-2"><ImArrowLeft2 /></button>
         <div className="font-bold">My Cart</div>
         <div className="text-gray-400">&#40;{cart.length}&#41;</div>
         <div className="border rounded-xl absolute right-6 p-2 text-white bg-royal-red flex gap-2 items-center">
-        <button className="font-bold">Checkout</button>
-        <FaLongArrowAltRight />
+        <div className="flex items-center gap-3 hover:scale-105 hover:transform transition:transform duration-300">
+          <button className="font-bold">Checkout</button>
+          <FaLongArrowAltRight />
+        </div>
       </div>
       </div>
       <CartItems cart={cart} />
